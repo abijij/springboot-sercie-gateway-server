4e1c8a9fe79e9ea60de03d4fe13c78e24a634639
abijij <105397465+abijij@users.noreply.github.com>
2024-02-03 23:04:39 -0600

    first commit

---
+++ src/main/java/com/abijij/springboot/app/gateway/springbootserciegatewayserver/SpringbootSercieGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.abijij.springboot.app.gateway.springbootserciegatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSercieGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSercieGatewayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abijij/springboot/app/gateway/springbootserciegatewayserver/SpringbootSercieGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abijij.springboot.app.gateway.springbootserciegatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSercieGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
